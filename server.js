161ca2ff4eb0d1689cd74e250c4dc2ec546b202e
rinshad21 <mdrinshadka@gmail.com>
2025-11-24 21:06:14 +0530

    cors update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://calitrac.vercel.app/"],
+    origin: ["http://localhost:5173", "https://calitrac.vercel.app"],
     credentials: true,
   })
 );
